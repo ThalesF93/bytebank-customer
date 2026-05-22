472881b12e19c27136aa295615d10fd502898ea7
izabela Velendes <izabelavelendes@MacBook-Air-de-izabela.local>
2026-05-22 11:47:40 -0300

    Documentacao de customers finalizada

--- src/main/java/br/com/bytebank/customers/api/dtos/responses/PagedResponse.java
+++ src/main/java/br/com/bytebank/customers/api/dtos/responses/PagedResponse.java
@@ -1,9 +1,13 @@
 package br.com.bytebank.customers.api.dtos.responses;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class PagedResponse<T> {
     private List<T> content;
     private int page;
@@ -20,43 +24,4 @@ public class PagedResponse<T> {
         this.totalPages = page.getTotalPages();
     }
 
-    public List<T> getContent() {
-        return content;
-    }
-
-    public void setContent(List<T> content) {
-        this.content = content;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public long getTotalElements() {
-        return totalElements;
-    }
-
-    public void setTotalElements(long totalElements) {
-        this.totalElements = totalElements;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
 }
--- src/main/java/br/com/bytebank/customers/api/openapi/controller/CustomerControllerOpenApi.java
+++ src/main/java/br/com/bytebank/customers/api/openapi/controller/CustomerControllerOpenApi.java
@@ -5,22 +5,84 @@ import br.com.bytebank.customers.api.dtos.requests.CustomerUpdateDTO;
 import br.com.bytebank.customers.api.dtos.responses.CustomerResponseDTO;
 import br.com.bytebank.customers.api.dtos.responses.CustomerShortResponseDTO;
 import br.com.bytebank.customers.api.dtos.responses.PagedResponse;
+import br.com.bytebank.customers.domain.exception.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.UUID;
 
+@Tag(name = "Customers")
 public interface CustomerControllerOpenApi {
 
-    ResponseEntity<CustomerResponseDTO> save(@Valid @RequestBody CustomerRequestDTO customerRequestDTO);
+    @Operation(summary = "Create Customer", description = "Creates a new customer into database")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Customer created successfully",
+                    content = @Content(schema = @Schema(implementation = CustomerResponseDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflict",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
 
-    PagedResponse<CustomerShortResponseDTO> getCostumers(@RequestParam(defaultValue = "0") int page,
-                                                         @RequestParam(defaultValue = "10") int size);
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request data",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<CustomerResponseDTO> save(@RequestBody(description = "Attributes required to Create a customer", required = true) CustomerRequestDTO customerRequestDTO);
 
-    ResponseEntity<CustomerUpdateDTO> updateCustomer(@PathVariable UUID id, @Valid @RequestBody CustomerUpdateDTO dto);
+    @Operation(summary = "Returns a list from all customers")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns a list or an empty list",
+                    content = @Content(schema = @Schema(implementation = CustomerShortResponseDTO.class))
+            )
+    })
+    PagedResponse<CustomerShortResponseDTO> getCostumers(@Parameter(description = "Parameter corresponding with the number of the page with defaultValue = 0") int page,
+                                                         @Parameter(description = "Inform how many elements the page may have" ) int size);
 
-    ResponseEntity<CustomerShortResponseDTO> findCustomerById(@PathVariable UUID id);
+    @Operation(description = "Update a customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Customer found and successfully updated",
+                    content = @Content(schema = @Schema(implementation = CustomerUpdateDTO.class))
+            ),
+
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Customer not found",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
+    ResponseEntity<CustomerUpdateDTO> updateCustomer(@Parameter (description = "Path variable that inform customer id", required = true) UUID id,
+                                                     @RequestBody (description = "DTO to inform to update customer", required = true) CustomerUpdateDTO dto);
+
+    @Operation(summary = "Find customer by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Customer found successfully",
+                    content = @Content(schema = @Schema(implementation = CustomerUpdateDTO.class))
+            ),
+
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Customer not found",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
+    ResponseEntity<CustomerShortResponseDTO> findCustomerById(@Parameter(description = "PathVariable to inform and find customer", required = true) UUID id);
 }
